ce04d502c0f16e60c8b219eb3a2f70ad500d9af8
monFries <143927221+monFries@users.noreply.github.com>
2024-11-14 15:02:35 +0100

    Update ipfs_scraper.py

--- ipfs_scraper.py
+++ ipfs_scraper.py
@@ -5,9 +5,8 @@ save_to_path = 'toys/cachedList.txt'
 
 def get_acelinks_ipfs():
 
-    #url = 'https://ipfs.io/ipns/k51qzi5uqu5dgg9al11vomikugim0o1i3l3fxp3ym3jwaswmy9uz8pq4brg1u9'
-    #url = 'https://ipfs.io/ipns/k51qzi5uqu5dgg9al11vomikugim0o1i3l3fxp3ym3jwaswmy9uz8pq4brg1u9'
-    url = 'https://bafybeihaak6jp7c6y3weyynkfruefwtgchvx2uuppmtts775k4z7kyw4ny.ipfs.dweb.link/'
+    url = 'https://ipfs.io/ipns/k51qzi5uqu5dgg9al11vomikugim0o1i3l3fxp3ym3jwaswmy9uz8pq4brg1u9'
+    #url = 'https://bafybeihaak6jp7c6y3weyynkfruefwtgchvx2uuppmtts775k4z7kyw4ny.ipfs.dweb.link/'
 
     '''
     # TOR USAGE. UNCOMMENT LINE INSIDE try/except BOLOCK:
